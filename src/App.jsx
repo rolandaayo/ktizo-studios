b2bf7b7eb6bc7a8686eb20a62f346c3536c18e1d
Roland Ibiwoye <contact@rolandayo.com>
2024-10-31 23:51:32 +0100

    rebuilding using react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/footer'
+import Hero from './components/hero'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
