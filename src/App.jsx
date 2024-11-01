5cfb0bdd0c181754e19eab9bdc33b0282e227cae
Roland Ibiwoye <contact@rolandayo.com>
2024-11-01 13:17:35 +0100

    hero update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Footer from './components/Footer'
-import Hero from './components/hero'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 function App() {
